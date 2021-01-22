5dc07939fa7d05b05c53c435b978d8263776f481
caishaohua <caishaohua@jarvis.net.cn>
2021-01-22 16:23:55 +0800

    autoMapper

---
+++ Routine.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Routine.Api.Services;
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/companies/{companyId}/employees")]
+    public class EmployeeController:ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ICompanyRepository companyRepository;
+
+        public EmployeeController(IMapper mapper,ICompanyRepository companyRepository)
+        {
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.companyRepository = companyRepository 
+                                    ?? throw new ArgumentNullException(nameof(companyRepository));
+        }
+    }
+}
