3b1e46ee60b81a64ee83b0e9321724d5472b745e
Cai <1572926739@qq.com>
2021-01-25 00:02:46 +0800

    父子资源

---
+++ Routine.Api/Migrations/20210124160110_AddEmployeeData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Routine.Api.Migrations
+{
+    public partial class AddEmployeeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CompanyId", "DateOfBirth", "EmployeeNo", "FirstName", "Gender", "LastName" },
+                values: new object[] { new Guid("6f5d5115-5f80-54bc-8c0f-8c52b28707d2"), new Guid("3a496508-384f-2fb2-5d1e-4e19328df49a"), new DateTime(1986, 11, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "G003", "Mary", 2, "King" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CompanyId", "DateOfBirth", "EmployeeNo", "FirstName", "Gender", "LastName" },
+                values: new object[] { new Guid("191bab05-ba96-e019-c86f-2a5b78ae12d2"), new Guid("c195d4f5-d469-c2b7-97bf-97cc8ff34b5f"), new DateTime(1977, 4, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "G097", "Kevin", 1, "Richardson" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CompanyId", "DateOfBirth", "EmployeeNo", "FirstName", "Gender", "LastName" },
+                values: new object[] { new Guid("85e19631-e8f0-2460-fe82-fed36d8905ef"), new Guid("09802ddf-f462-d23f-88b5-b6991e7cd6c9"), new DateTime(1977, 4, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "G098", "Kevin", 2, "Richardson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: new Guid("191bab05-ba96-e019-c86f-2a5b78ae12d2"));
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: new Guid("6f5d5115-5f80-54bc-8c0f-8c52b28707d2"));
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: new Guid("85e19631-e8f0-2460-fe82-fed36d8905ef"));
+        }
+    }
+}
