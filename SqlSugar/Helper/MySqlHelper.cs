b13272572fe8a0cf4eb0ba840adf300ff450ae2a
caishaohua <1572926739@qq.com>
2021-02-01 15:35:42 +0800

    dd

---
+++ SqlSugar/Helper/MySqlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace SqlSugar.Helper
+{
+    public class MySqlHelper
+    {
+        private readonly SqlSugarClient _client;
+        public MySqlHelper(string connectionStr)
+        {
+            _client = new SqlSugarClient(new ConnectionConfig
+            {
+                ConnectionString = connectionStr,
+                DbType = DbType.MySql,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute
+            });
+
+            CheckEntitiesFolder();
+
+        }
+
+        private void CheckEntitiesFolder()
+        {
+            if (Directory.GetDirectories(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase ?? string.Empty,
+                "Entities")).Length == 0)
+            {
+                _client.DbFirst.CreateClassFile(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,"Entities");
+            }
+        }
+        
+    }
+}
--- SqlSugar/Program.cs
+++ SqlSugar/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-
+using Microsoft.Extensions.Configuration;
+using SqlSugar.Helper;
 namespace SqlSugar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            //Console.WriteLine("Hello World!");
+            var config = ConfigHelper.AddConfigFiles();
+
+            var connectionStr = config.GetConnectionString("MySqlConnection");
+
+            var sqlHelper=new MySqlHelper(connectionStr);
             Console.ReadKey();
         }
     }
